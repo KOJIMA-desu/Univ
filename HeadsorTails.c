60fbf4546d392c90550c68996773ab70fcd3ae62
小島大輝 <kojimadaiki@kojimadaikinoMacBook-Air.local>
2020-08-06 23:40:43 +0900

    Hot:Initial revision

---
+++ HeadsorTails.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Uniform()
+{
+    return ((double)rand() + 1.0) / ((double)RAND_MAX + 2.0);
+}
+
+int main()
+{
+    int Heads = 0, Tails = 0;
+    int a;
+
+    printf("tossing coin...\n");
+    for (int i = 1; i < 4; i++)
+    {
+        printf("round %d:", i);
+        if (Uniform() % 2 == 0)
+        {
+            printf("Heads\n");
+            Heads++;
+        }
+        else
+        {
+            printf("Tails\n");
+            Tails++;
+        }
+    }
+    printf("Heads: %d, Tails: %d\n", Heads, Tails);
+    return 0;
+}
